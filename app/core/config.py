070bc3c34a0ed0f02deb44ba660b1a9ee1eb1441
darkgooddack <ulyanaworkpage@gmail.com>
2025-02-23 15:55:01 +0300

    поправила config и библиотеку bcrypt поменяла на версию 4.0.1

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,21 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
-    REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
-    REDIS_DB: int = int(os.getenv("REDIS_DB", 0))
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ALGORITHM: str = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    load_dotenv()
+
+    DATABASE_URL: str
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 settings = Settings()
-
